9bfb7bbcfe096da40394434c4c15ccc57def7106
AlexHls <alex.holas@gmx.de>
2021-12-06 17:25:56 +0100

    Added initial condition plotting script

--- arepo_to_tardis.py
+++ arepo_to_tardis.py
@@ -13,12 +13,12 @@ import sys
 
 
 def get_max_species(snapshot, numspecies=5):
-    
+
     maxind = []
     frac = []
-    
+
     for i in range(s.nspecies):
-        frac.append(s.data["xnuc"][:,i].sum()/msol)
+        frac.append(s.data["xnuc"][:, i].sum() / msol)
 
     while len(maxind) < numspecies:
         for i in range(len(frac)):
@@ -28,96 +28,144 @@ def get_max_species(snapshot, numspecies=5):
                 maxind.append(i)
                 frac[i] = 0
                 break
-    
+
     return maxind
 
 
-def mapOnCartGridNuc( snapshot, value, ind, center=False, box=False, res=512, saveas=False, use_only_cells=None, numthreads=1):
-        if type( center ) == list:
-            center = pylab.array( center )
-        elif type( center ) != np.ndarray:
-            center = snapshot.center
+def mapOnCartGridNuc(
+    snapshot,
+    value,
+    ind,
+    center=False,
+    box=False,
+    res=512,
+    saveas=False,
+    use_only_cells=None,
+    numthreads=1,
+):
+    if type(center) == list:
+        center = pylab.array(center)
+    elif type(center) != np.ndarray:
+        center = snapshot.center
 
-        if type( box ) == list:
-            box = pylab.array( box )
-        elif type( box ) != np.ndarray:
-            box = np.array( [snapshot.boxsize,snapshot.boxsize,snapshot.boxsize] )
+    if type(box) == list:
+        box = pylab.array(box)
+    elif type(box) != np.ndarray:
+        box = np.array([snapshot.boxsize, snapshot.boxsize, snapshot.boxsize])
 
-        if type( res ) == list:
-            res = pylab.array( res )
-        elif type( res ) != np.ndarray:
-            res = np.array( [res]*3 )
-            
-        if use_only_cells is None:
-            use_only_cells = np.arange( snapshot.nparticlesall[0], dtype='int32' )
+    if type(res) == list:
+        res = pylab.array(res)
+    elif type(res) != np.ndarray:
+        res = np.array([res] * 3)
 
-        pos = snapshot.pos[use_only_cells,:].astype( 'float64' )
-        px = np.abs( pos[:,0] - center[0] )
-        py = np.abs( pos[:,1] - center[1] )
-        pz = np.abs( pos[:,2] - center[2] )
+    if use_only_cells is None:
+        use_only_cells = np.arange(snapshot.nparticlesall[0], dtype="int32")
 
-        pp, = np.where( (px < 0.5*box[0]) & (py < 0.5*box[1]) & (pz < 0.5*box[2]) )
-        print("Selected %d of %d particles." % (pp.size,snapshot.npart))
+    pos = snapshot.pos[use_only_cells, :].astype("float64")
+    px = np.abs(pos[:, 0] - center[0])
+    py = np.abs(pos[:, 1] - center[1])
+    pz = np.abs(pos[:, 2] - center[2])
 
-        posdata = pos[pp]
-        valdata = snapshot.data[value][use_only_cells,ind][pp].astype('float64')
-        
-        if valdata.ndim == 1:
-            data = calcGrid.calcASlice(posdata, valdata, nx=res[0], ny=res[1], nz=res[2], boxx=box[0], boxy=box[1], boxz=box[2],
-                                       centerx=center[0], centery=center[1], centerz=center[2], grid3D=True, numthreads=numthreads)
-            grid = data["grid"]
-        else:
-            # We are going to generate ndim 3D grids and stack them together
-            # in a grid of shape (valdata.shape[1],res,res,res)
-            grid = []
-            for dim in range(valdata.shape[1]):
-                data = calcGrid.calcASlice(posdata, valdata[:,dim], nx=res[0], ny=res[1], nz=res[2], boxx=box[0], boxy=box[1], boxz=box[2],
-                                           centerx=center[0], centery=center[1], centerz=center[2], grid3D=True, numthreads=numthreads)
-                grid.append(data["grid"])
-            grid = np.stack([subgrid for subgrid in grid])
-        if saveas:
-            grid.tofile( saveas )
+    (pp,) = np.where((px < 0.5 * box[0]) & (py < 0.5 * box[1]) & (pz < 0.5 * box[2]))
+    print("Selected %d of %d particles." % (pp.size, snapshot.npart))
 
-        return grid
+    posdata = pos[pp]
+    valdata = snapshot.data[value][use_only_cells, ind][pp].astype("float64")
+
+    if valdata.ndim == 1:
+        data = calcGrid.calcASlice(
+            posdata,
+            valdata,
+            nx=res[0],
+            ny=res[1],
+            nz=res[2],
+            boxx=box[0],
+            boxy=box[1],
+            boxz=box[2],
+            centerx=center[0],
+            centery=center[1],
+            centerz=center[2],
+            grid3D=True,
+            numthreads=numthreads,
+        )
+        grid = data["grid"]
+    else:
+        # We are going to generate ndim 3D grids and stack them together
+        # in a grid of shape (valdata.shape[1],res,res,res)
+        grid = []
+        for dim in range(valdata.shape[1]):
+            data = calcGrid.calcASlice(
+                posdata,
+                valdata[:, dim],
+                nx=res[0],
+                ny=res[1],
+                nz=res[2],
+                boxx=box[0],
+                boxy=box[1],
+                boxz=box[2],
+                centerx=center[0],
+                centery=center[1],
+                centerz=center[2],
+                grid3D=True,
+                numthreads=numthreads,
+            )
+            grid.append(data["grid"])
+        grid = np.stack([subgrid for subgrid in grid])
+    if saveas:
+        grid.tofile(saveas)
+
+    return grid
 
 
 def write_csvy_model(expdict, exportname, density_day, isotope_day, specieslist=[]):
-    
+
     with open(exportname, "a") as f:
         # WRITE HEADER
-        f.write("".join([
-            "---\n",
-            "name: csvy_full\n",
-            "model_density_time_0: %d day\n" % density_day,
-            "model_isotope_time_0: %d day\n" % isotope_day,
-            "description: Config file for TARDIS from Arepo snapshot.\n",
-            "tardis_model_config_version: v1.0\n",
-            "datatype:\n",
-            "  fields:\n",
-            "    -  name: velocity\n",
-            "       unit: km/s\n",
-            "       desc: velocities of shell outer bounderies.\n",
-            "    -  name: density\n",
-            "       unit: g/cm^3\n",
-            "       desc: density of shell.\n",
-            #"    -  name: t_rad\n",
-            #"       unit: K\n",
-            #"       desc: radiative temperature.\n",
-            #"    -  name: dilution_factor\n",
-            #"       desc: dilution factor of shell.\n",
-        ]))
+        f.write(
+            "".join(
+                [
+                    "---\n",
+                    "name: csvy_full\n",
+                    "model_density_time_0: %d day\n" % density_day,
+                    "model_isotope_time_0: %d day\n" % isotope_day,
+                    "description: Config file for TARDIS from Arepo snapshot.\n",
+                    "tardis_model_config_version: v1.0\n",
+                    "datatype:\n",
+                    "  fields:\n",
+                    "    -  name: velocity\n",
+                    "       unit: km/s\n",
+                    "       desc: velocities of shell outer bounderies.\n",
+                    "    -  name: density\n",
+                    "       unit: g/cm^3\n",
+                    "       desc: density of shell.\n",
+                    # "    -  name: t_rad\n",
+                    # "       unit: K\n",
+                    # "       desc: radiative temperature.\n",
+                    # "    -  name: dilution_factor\n",
+                    # "       desc: dilution factor of shell.\n",
+                ]
+            )
+        )
 
         for species in specieslist:
-            f.write("".join([
-                "    -  name: %s\n" % species,
-                "       desc: fractional %s abundance.\n" % species,
-            ]))
+            f.write(
+                "".join(
+                    [
+                        "    -  name: %s\n" % species,
+                        "       desc: fractional %s abundance.\n" % species,
+                    ]
+                )
+            )
 
-        f.write("".join([
-            "\n",
-            "---\n",
-        ]))
-        
+        f.write(
+            "".join(
+                [
+                    "\n",
+                    "---\n",
+                ]
+            )
+        )
+
         # WRITE DATA
         datastring = ["velocity,", "density,"]
         for species in specieslist[:-1]:
@@ -134,147 +182,184 @@ def write_csvy_model(expdict, exportname, density_day, isotope_day, specieslist=
             f.write("%f" % expdict[keylist[-1]][i])
             f.write("\n")
 
-    return exportname 
+    return exportname
 
 
 def euler_to_rotmat(alpha, beta, gamma):
-    rz_yaw = np.array([
-        [np.cos(alpha), -np.sin(alpha), 0],
-        [np.sin(alpha),  np.cos(alpha), 0],
-        [          0,            0, 1]])
-    ry_pitch = np.array([
-        [ np.cos(beta), 0, np.sin(beta)],
-        [            0, 1,            0],
-        [-np.sin(beta), 0, np.cos(beta)]])
-    rx_roll = np.array([
-        [1,             0,              0],
-        [0, np.cos(gamma), -np.sin(gamma)],
-        [0, np.sin(gamma),  np.cos(gamma)]])
+    rz_yaw = np.array(
+        [
+            [np.cos(alpha), -np.sin(alpha), 0],
+            [np.sin(alpha), np.cos(alpha), 0],
+            [0, 0, 1],
+        ]
+    )
+    ry_pitch = np.array(
+        [[np.cos(beta), 0, np.sin(beta)], [0, 1, 0], [-np.sin(beta), 0, np.cos(beta)]]
+    )
+    rx_roll = np.array(
+        [
+            [1, 0, 0],
+            [0, np.cos(gamma), -np.sin(gamma)],
+            [0, np.sin(gamma), np.cos(gamma)],
+        ]
+    )
     # R = RzRyRx
     rotmat = np.dot(rz_yaw, np.dot(ry_pitch, rx_roll))
     return rotmat
 
 
-def arepo_to_tardis(snapshot, alpha=0, beta=0, gamma=0, boxsize=0.4, resolution=512, shells=50, speciesfile="species55.txt", numspecies=5, numthreads=4, export=""):
-    
-    mpl.rcParams['axes.prop_cycle'] = cycler(color=[
-        '#1f77b4', '#1f77b4',
-        '#ff7f0e', '#ff7f0e',
-        '#2ca02c', '#2ca02c',
-        '#d62728', '#d62728',
-        '#9467bd', '#9467bd', 
-        '#8c564b', '#8c564b', 
-        '#e377c2', '#e377c2', 
-        '#7f7f7f', '#7f7f7f', 
-        '#bcbd22', '#bcbd22', 
-        '#17becf', '#17becf',
-        ])
+def arepo_to_tardis(
+    snapshot,
+    alpha=0,
+    beta=0,
+    gamma=0,
+    boxsize=0.4,
+    resolution=512,
+    shells=50,
+    speciesfile="species55.txt",
+    numspecies=5,
+    numthreads=4,
+    export="",
+):
 
-    expname = "%s_converted_to_csvy" % export 
+    mpl.rcParams["axes.prop_cycle"] = cycler(
+        color=[
+            "#1f77b4",
+            "#1f77b4",
+            "#ff7f0e",
+            "#ff7f0e",
+            "#2ca02c",
+            "#2ca02c",
+            "#d62728",
+            "#d62728",
+            "#9467bd",
+            "#9467bd",
+            "#8c564b",
+            "#8c564b",
+            "#e377c2",
+            "#e377c2",
+            "#7f7f7f",
+            "#7f7f7f",
+            "#bcbd22",
+            "#bcbd22",
+            "#17becf",
+            "#17becf",
+        ]
+    )
+
+    expname = "%s_converted_to_csvy" % export
 
     values = [
-            "vel",
-            "rho",
-            "xnuc",
-        ]
+        "vel",
+        "rho",
+        "xnuc",
+    ]
 
     max_species = get_max_species(snapshot, numspecies=numspecies)
     species = np.genfromtxt(speciesfile, skip_header=1, dtype=str).T[0]
-    
+
     specieslist = []
-    
+
     expdict = {}
 
-    midpoint = int(np.ceil(resolution/2))
-    
+    midpoint = int(np.ceil(resolution / 2))
+
     # Rotate snapshot using Euler angles
-    rotmat = euler_to_rotmat(alpha, beta, gamma) 
+    rotmat = euler_to_rotmat(alpha, beta, gamma)
     s.rotateto(rotmat[0], dir2=rotmat[1], dir3=rotmat[2])
 
     # Extract profiles along (1,0,0).T axis
     for value in values:
         if value != "xnuc":
             cartesian_map = snapshot.mapOnCartGrid(
-                    value, 
-                    box=[boxsize*rsol, boxsize*rsol, boxsize*rsol],
-                    center=snapshot.centerofmass(),
-                    res=resolution,
-                    numthreads=numthreads,
-                )
+                value,
+                box=[boxsize * rsol, boxsize * rsol, boxsize * rsol],
+                center=snapshot.centerofmass(),
+                res=resolution,
+                numthreads=numthreads,
+            )
 
             profile = np.array(cartesian_map)
-            
+
             if value == "vel":
                 profile = np.sqrt(
-                        profile[0, midpoint, midpoint:, midpoint] ** 2
-                        + profile[1, midpoint, midpoint:, midpoint] ** 2
-                        + profile[2, midpoint, midpoint:, midpoint] ** 2
-                    )
+                    profile[0, midpoint, midpoint:, midpoint] ** 2
+                    + profile[1, midpoint, midpoint:, midpoint] ** 2
+                    + profile[2, midpoint, midpoint:, midpoint] ** 2
+                )
             else:
                 profile = profile[midpoint, midpoint:, midpoint]
 
-            
-            x = np.arange(0,len(profile))
+            x = np.arange(0, len(profile))
 
             f = interpolate.interp1d(x, profile)
 
             shellpoints = np.linspace(0, max(x), shells)
-            
+
             profile_interpolated = f(shellpoints)
 
-            plt.plot(shellpoints, profile_interpolated/max(profile_interpolated), '--', label=value+"_interp")
-            plt.plot(x, profile/max(profile), label=value)
+            plt.plot(
+                shellpoints,
+                profile_interpolated / max(profile_interpolated),
+                "--",
+                label=value + "_interp",
+            )
+            plt.plot(x, profile / max(profile), label=value)
 
             expdict[value] = profile_interpolated
 
         else:
             for s in max_species:
                 cartesian_map = mapOnCartGridNuc(
-                        snapshot,
-                        value,
-                        s,
-                        box=[boxsize*rsol, boxsize*rsol, boxsize*rsol],
-                        center=snapshot.centerofmass(),
-                        res=resolution,
-                        numthreads=numthreads,
-                    )
+                    snapshot,
+                    value,
+                    s,
+                    box=[boxsize * rsol, boxsize * rsol, boxsize * rsol],
+                    center=snapshot.centerofmass(),
+                    res=resolution,
+                    numthreads=numthreads,
+                )
 
                 profile = np.array(cartesian_map)
-                
+
                 profile = profile[midpoint, midpoint:, midpoint]
 
-                x = np.arange(0,len(profile))
+                x = np.arange(0, len(profile))
 
                 f = interpolate.interp1d(x, profile)
 
                 shellpoints = np.linspace(0, max(x), shells)
-                
+
                 profile_interpolated = f(shellpoints)
-                
+
                 species_name = species[s].capitalize()
 
-                plt.plot(shellpoints, profile_interpolated, '--', label=species_name+"_interp")
+                plt.plot(
+                    shellpoints,
+                    profile_interpolated,
+                    "--",
+                    label=species_name + "_interp",
+                )
                 plt.plot(x, profile, label=species_name)
-                
+
                 specieslist.append(species_name)
                 expdict[species_name] = profile_interpolated
 
-
     plt.legend()
     plt.savefig(
-            expname + ".pdf",
-            bbox_inches="tight",
-            dpi=600,
-        )
+        expname + ".pdf",
+        bbox_inches="tight",
+        dpi=600,
+    )
     plt.close()
 
     # Normalise nuclear abundance fractions to sum to one
     for i in range(len(expdict[specieslist[0]])):
         a = 0
         for k in specieslist:
-            a += expdict[k][i] 
+            a += expdict[k][i]
         for k in specieslist:
-            expdict[k][i] = expdict[k][i]/a
+            expdict[k][i] = expdict[k][i] / a
 
     print(list(expdict.keys()))
 
@@ -314,4 +399,6 @@ if __name__ == "__main__":
 
     s = gadget_readsnap(snapshot)
 
-    arepo_to_tardis(s, export=str(snapshot), alpha=alpha, beta=beta, gamma=gamma, shells=5)
+    arepo_to_tardis(
+        s, export=str(snapshot), alpha=alpha, beta=beta, gamma=gamma, shells=5
+    )
---
+++ plot_ic.py
@@ -0,0 +1,204 @@
+import sys
+import os
+import glob
+import subprocess
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from loadmodules import *
+import gadget_snap
+from const import rsol, msol
+from matplotlib import rcParams
+
+
+def plot_ic(
+    file,
+    value,
+    vrange,
+    scale,
+    boxsize=0.3,
+    scale_center=0.80,
+    axes=[0, 1],
+    logplot=True,
+    clean=False,
+    numthreads=4,
+):
+    filename_dict = {
+        "rho": "density",
+        "bfld": "bfld",
+        "pass00": "pass00",
+        "mass": "mass",
+        "vel": "vel",
+        "mach": "mach",
+        "pres": "pres",
+        "pb": "pb",
+        "temp": "temp",
+        "u": "u",
+        "pass01": "pass01",
+        "xnuc00": "Hefrac",
+        "xnuc01": "Cfrac",
+    }
+    units_dict = {
+        "rho": r"Density ($g/cm^3$)",
+        "mass": r"Mass ($g$)",
+        "bfld": r"Bfield (G)",
+        "vel": r"Velocity ($m/,s^{-1}$)",
+        "mach": "Mach Number",
+        "pres": "Pressure",
+        "pb": "$P_B/P_{gas}$",
+        "temp": "Temperature (K)",
+        "u": "Internal Energy (erg)",
+        "pass00": "Passive Scalar",
+        "pass01": "Passive Scalar",
+        "xnuc00": "He fraction",
+        "xnuc01": "C fraction",
+    }
+
+    s = gadget_snap.gadget_snapshot(file, hdf5=True, quiet=True, lazy_load=True)
+
+    if vrange == None:
+        pc = s.plot_Aslice(
+            value,
+            axes=axes,
+            logplot=logplot,
+            colorbar=True,
+            box=[boxsize * rsol, boxsize * rsol],
+            proj=True,
+            numthreads=numthreads,
+            center=s.centerofmass(),
+            cmap="cescale",
+        )
+    else:
+        pc = s.plot_Aslice(
+            value,
+            axes=axes,
+            logplot=logplot,
+            colorbar=True,
+            vrange=vrange,
+            box=[boxsize * rsol, boxsize * rsol],
+            proj=True,
+            numthreads=numthreads,
+            center=s.centerofmass(),
+            cmap="cescale",
+        )
+
+    fig = pc.get_figure()
+    ax = fig.get_axes()
+
+    xlim = ax[0].get_xlim()
+    size = xlim[1] - xlim[0]
+    rel_size = scale / size
+    scale_pos = (
+        (scale_center - rel_size / 2.0, scale_center + rel_size / 2.0),
+        (0.05, 0.05),
+    )
+
+    if not clean:
+        line = ax[0].add_line(
+            plt.Line2D(
+                scale_pos[0],
+                scale_pos[1],
+                linewidth=2,
+                color="w",
+                label=r"$" + str(scale / rsol) + "R_\odot$",
+                transform=ax[0].transAxes,
+            )
+        )
+        ax[0].text(
+            np.mean(scale_pos[0]),
+            np.mean(scale_pos[1]),
+            r"$" + str(scale / rsol) + "\,R_\odot$",
+            verticalalignment="bottom",
+            horizontalalignment="center",
+            transform=ax[0].transAxes,
+            color="w",
+        )
+        ax[1].set_ylabel(units_dict[value])
+        ax[1].set_position([0.75, 0, 1, 1])
+
+    ax[0].tick_params(
+        axis=u"both", which=u"both", length=0, labelbottom=False, labelleft=False
+    )
+    ax[0].yaxis.offsetText.set_visible(False)
+    ax[0].xaxis.offsetText.set_visible(False)
+    ax[0].set_position([0, 0, 0.8, 1])
+
+    if clean:
+        fig.delxes(ax[1])
+        ax[0].set_position([0, 0, 1, 1])
+        fig.savefig(
+            file.relace(".hdf5", "_%s.pdf" % filename_dict[value]),
+            dpi=600,
+            pad_inches=0,
+            bbox_inches="tight",
+        )
+    else:
+        fig.savefig(
+            file.replace(".hdf5", "_%s.pdf" % filename_dict[value]),
+            dpi=600,
+        )
+
+    plt.close(fig)
+
+    return None
+
+
+def main(
+    file,
+    value,
+    vrange=None,
+    axes=[0, 1],
+    boxsize=0.2,
+    logplot=True,
+    clean=False,
+    scale=None,
+):
+
+    units_dict = {
+        "rho": r"Density ($g/cm^3$)",
+        "mass": r"Mass ($g$)",
+        "bfld": r"Bfield (G)",
+        "vel": r"Velocity ($m/,s^{-1}$)",
+        "mach": "Mach Number",
+        "pres": "Pressure",
+        "pb": "$P_B/P_{gas}$",
+        "temp": "Temperature (K)",
+        "u": "Internal Energy (erg)",
+        "pass00": "Passive Scalar",
+        "pass01": "Passive Scalar",
+        "xnuc00": "He fraction",
+        "xnuc01": "C fraction",
+    }
+
+    if value not in list(units_dict.keys()):
+        print("Value %s not recognized, ignoring..." % value)
+        return
+
+    if scale is None:
+        scale = boxsize / 5 * rsol
+
+    rcParams["font.family"] = "Roboto"
+
+    print("Plotting value", value)
+
+    plot_ic(file, value, vrange, scale)
+
+    print("Finished plotting value", value)
+
+    return
+
+
+if __name__ == "__main__":
+
+    file = sys.argv[1]
+
+    if not os.path.exists(file):
+        sys.exit("Specified file does not exist! Aborting...")
+
+    values = sys.argv[2:]
+
+    for value in values:
+        main(file, value)
+
+    print("---FINISHED PLOTTING INITIAL CONDITIONS---")
