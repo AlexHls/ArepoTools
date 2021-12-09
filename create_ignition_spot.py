caf376c7452c469477bb2e428b6e6c7ef48db81d
AlexHls <alex.holas@gmx.de>
2021-12-09 13:50:37 +0100

    Added ignition spot script

---
+++ create_ignition_spot.py
@@ -0,0 +1,170 @@
+#!/home/alexh/anaconda3/envs/arepo/bin/python
+
+import sys
+import os
+import argparse
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from loadmodules import *
+import gadget_snap
+from gadget import gadget_write_ics
+from const import rsol, msol
+
+
+def set_ignition_energy(snapshot, eos, temp, ind):
+
+    u_new = eos.tgiven(
+        np.array(snapshot.data["rho"][ind]), np.array(snapshot.data["xnuc"][ind]), temp
+    )[0]
+
+    snapshot.data["u"][ind] = u_new
+    snapshot.data["temp"][ind] = temp
+
+    return snapshot
+
+
+def create_ignition_spot(
+    file,
+    temp=3e9,
+    ign_rad=0,
+    ign_phi=0,
+    ign_theta=0,
+    num_ignition_cells=100,
+    outname=None,
+    eos_file=None,
+    species_file=None,
+):
+
+    # Read snapshot
+    if os.path.exists(file):
+        s = gadget_snap.gadget_snapshot(file, hdf5=True, quiet=True, lazy_load=True)
+    else:
+        raise FileNotFoundError("Snapshot file not found")
+
+    # Fallback to default EoS and species datafile if not set
+    if eos_file is None:
+        eos_file = "helm_table.dat"
+    if species_file is None:
+        species_file = "species55.txt"
+
+    # Create EoS
+    if not os.path.exists(eos_file):
+        raise FileNotFoundError("EOS datafile not found")
+    elif not os.path.exists(species_file):
+        raise FileNotFoundError("Species file not found")
+    else:
+        eos = loadhelm_eos(eos_file, species_file)
+
+    # Convert coordinates into center of mass frame
+    pos_com = np.array(s.data["pos"] - s.centerofmass())
+
+    # Calculate cartesian coordinates of desired ignition spot
+    pos_ign = np.array(
+        [
+            ign_rad * np.cos(ign_phi) * np.sin(ign_theta),
+            ign_rad * np.sin(ign_phi) * np.sin(ign_theta),
+            ign_rad * np.cos(ign_theta),
+        ]
+    )
+
+    # Calculate distance between cell com-coordinates and ignition spot
+    dist = np.sqrt(
+        (pos_com[:, 0] - pos_ign[0]) ** 2
+        + (pos_com[:, 1] - pos_ign[1]) ** 2
+        + (pos_com[:, 2] - pos_ign[2]) ** 2
+    )
+
+    # For the num_ignition_cells cells with the smallest distance to the ignition
+    # spot, set the internal energy to match the specified temperature
+    min_inds = np.argsort(dist)
+
+    for i in range(num_ignition_cells):
+        set_ignition_energy(s, eos, temp, min_inds[i])
+
+    # Write resulting snapshot as new initial condition file
+    if outname is None:
+        print("No outname specified, disregarding results...")
+    else:
+        if ".hdf5" in outname:
+            outname = outname.replace(".hdf5", "")
+
+        data = s.data
+        gadget_write_ics(outname, data, double=True, format="hdf5")
+
+    return
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "snapshot",
+        help="Paht to the snapshot file for which an ignition spot is to be created.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="Output file to which the modified snapshot is to be saved.",
+    )
+    parser.add_argument(
+        "-e",
+        "--eos",
+        help="Path to EoS datafile. Only has to be specified when datafile is not in the working directory.",
+    )
+    parser.add_argument(
+        "-s",
+        "--species",
+        help="Path to species datafile. Only has to be specified when datafile is not in the working directory.",
+    )
+    parser.add_argument(
+        "-k",
+        "--temperature",
+        help="Temperature of the ignition spot in K. Default: 3e9.",
+        type=float,
+        default=3e9,
+    )
+    parser.add_argument(
+        "-n",
+        "--num_cells",
+        help="Number of cells with which to create ignition spot. Default: 100.",
+        type=int,
+        default=100,
+    )
+    parser.add_argument(
+        "-r",
+        "--radius",
+        help="Radial distance of the ignition center to the center of mass. Refers to spherical coordinates. Default: 0",
+        type=float,
+        default=0,
+    )
+    parser.add_argument(
+        "-p",
+        "--phi",
+        help="Polar angle of the ignition center to the center of mass. Refers to spherical coordinates. Default: 0",
+        type=float,
+        default=0,
+    )
+    parser.add_argument(
+        "-t",
+        "--theta",
+        help="Azimuthal angle of the ignition center to the center of mass. Refers to spherical coordinates. Default: 0",
+        type=float,
+        default=0,
+    )
+
+    args = parser.parse_args()
+
+    create_ignition_spot(
+        args.snapshot,
+        outname=args.output,
+        eos_file=args.eos,
+        species_file=args.species,
+        temp=args.temperature,
+        num_ignition_cells=args.num_cells,
+        ign_rad=args.radius,
+        ign_phi=args.phi,
+        ign_theta=args.theta,
+    )
